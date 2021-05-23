f629880c4afc7f62ddfefa75532f4c1cd26c4662
Suxhita <81512383+Suxhita@users.noreply.github.com>
2021-05-23 23:41:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var one = Suchita;
+console.log(one);
+var two = Arul;
+console.log(two);
+var fullName = one.concat(two);
+console.log(fullName);
+
+var no1 = 875
+console.log(no1)
+var no2 = 8264
+console.log(no2)
+var ans = no1 + no2
+console.log(ans)
